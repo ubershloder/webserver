4e60269d1fdaced86778615c224050753618a7f8
Ivan Karpenko <ubersholder@gmail.com>
2021-07-17 21:02:05 +0300

    pipeline

--- EC instance.tf
+++ EC instance.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "eu-central-1"
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
