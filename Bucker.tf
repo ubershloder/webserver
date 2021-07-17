25af93b736f3129185dbf5396af02dd644a47b9c
Ivan Karpenko <ubersholder@gmail.com>
2021-07-17 15:36:20 +0300

    pipeline

--- Bucker.tf
+++ Bucker.tf
@@ -1,7 +1,6 @@
 resource "aws_s3_bucket" "b" {
   bucket = "uber-self-gitlab-serverless"
   acl    = "public-read"
-  policy = file("policy.json")
 
   website {
     index_document = "frontend.html"
